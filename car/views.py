5754eda491ec04da00c6c7149799b329908f01d9
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-12-08 21:51:40 +0100

    add CarListView class with search_form

--- car/views.py
+++ car/views.py
@@ -1,5 +1,36 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from car.forms import CarSearchWinForm
+from car.models import Car
 
 
 def index(request):
     return render(request, "car/index.html")
+
+
+class CarListView(LoginRequiredMixin, ListView):
+    model = Car
+    paginate_by = 1
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CarListView, self).get_context_data(**kwargs)
+        win_code = self.request.GET.get("win_code", "")
+        context["search_form"] = CarSearchWinForm(
+            initial={"win_code": win_code}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Car.objects.all()
+        form = CarSearchWinForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                win_code=form.cleaned_data["win_code"]
+            )
+        return queryset
+
+
+class CarDetailView(LoginRequiredMixin, DetailView):
+    model = Car
