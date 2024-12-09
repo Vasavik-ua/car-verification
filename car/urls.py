e2486348aea4ddb1ce4490880e07c7cc17737a94
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-12-09 21:48:03 +0100

    add url for CarDetailView

--- car/urls.py
+++ car/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from car.views import index, CarListView
+from car.views import index, CarListView, CarDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path("car/", CarListView.as_view(), name="car_list"),
+    path("car/<int:pk>", CarDetailView.as_view(), name="car-detail")
 
 ]
 
