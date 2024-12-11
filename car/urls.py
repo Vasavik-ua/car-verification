33218511bd40eae9cfe21706875e0f26a6b18b63
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-12-11 22:21:31 +0100

    add path car-create

--- car/urls.py
+++ car/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from car.views import index, CarListView, CarDetailView, CarUpdateView
+from car.views import index, CarListView, CarDetailView, CarUpdateView, CarDeleteView, CarCreateView
 
 urlpatterns = [
     path("", index, name="index"),
     path("car/", CarListView.as_view(), name="car_list"),
     path("car/<int:pk>", CarDetailView.as_view(), name="car-detail"),
-    path("car/<int:pk>/update/", CarUpdateView.as_view(), name="car-update")
+    path("car/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("car/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path("car/create/", CarCreateView.as_view(), name="car-create")
 
 ]
 
