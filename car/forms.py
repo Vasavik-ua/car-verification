f915e6be0cc4b4422800adefb5b8ed56d8afeec6
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-12-12 21:33:52 +0100

    add new CarSearchCheckUp form for search dy field:performed_by

--- car/forms.py
+++ car/forms.py
@@ -10,3 +10,14 @@ class CarSearchWinForm(forms.Form):
             "placeholder": "Search by WIN"
         })
     )
+
+
+class CarSearchCheckUp(forms.Form):
+    performed_by = forms.CharField(
+        max_length=255,
+        required=True,
+        label="",
+        widget=forms.TextInput(attrs={
+            "placeholder": "Search by Performed By"
+        })
+    )
