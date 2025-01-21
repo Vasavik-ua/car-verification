97a53662d688d43dbb54cbe5a4ddaf7e1eca4b3f
Vasavik-ua <47318025+Vasavik-ua@users.noreply.github.com>
2025-01-21 22:00:47 +0100

    Update dev.py

--- car_verification/settings/dev.py
+++ car_verification/settings/dev.py
@@ -1,9 +1,9 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "car-verification.onrender.com"]
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
